77b25edb57060a946dd2b2948c8da94b73ca1d7e
wakatsuki <wakatsuki@PC-0120>
2018-08-06 14:49:04 +0900

    再うp

---
+++ src/main/java/com/example/Human/entity/Products.java
@@ -0,0 +1,15 @@
+package com.example.Human.entity;
+
+public class Products {
+	
+	String product_name;
+	
+	public String getProduct_name() {
+		return product_name;
+	}
+	
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+
+}
