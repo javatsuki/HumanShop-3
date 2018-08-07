f120eefbc96de417dd9d6647ab919e1d9a1fa884
itsuki_endo <itsuki_endo@DTPC-PEO-005>
2018-08-07 11:28:51 +0900

    ログイン画面作成しましたよーーーーーー

---
+++ src/main/java/com/example/Human/Controller/LoginController.java
@@ -0,0 +1,24 @@
+package com.example.Human.Controller;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+  public class LoginController { 
+  	 
+  	private static final Logger logger = LoggerFactory.getLogger(LoginController.class); 
+  	 
+  	/** 
+  	 * Simply selects the home view to render by returning its name. 
+  	 */ 
+ 	@RequestMapping(value = "/Login") 
+  	public String home(Locale locale, Model model) { 
+  		 
+  		return "Login"; 
+  	}	 
+  } 
