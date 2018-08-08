cb1426bf57c090b336cfadf04b7eb1db756b7a86
wakatsuki <wakatsuki@PC-0120>
2018-08-08 18:44:46 +0900

    kkkk

--- src/main/java/com/example/Human/entity/Users.java
+++ src/main/java/com/example/Human/entity/Users.java
@@ -2,38 +2,49 @@ package com.example.Human.entity;
 
 public class Users {
 	
-	//address1の表示
-	String address1;
-	
-	public String getAddress1() {
-		return address1;
+	private String user_id;
+	private String user_name;
+	private String password;
+	private String address1;
+	private String address2;
+	private String address3;
+	public String getUser_id() {
+		return user_id;
 	}
-
-	public void setAddress1(String address1) {
-		this.address1 = address1;
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
 	}
-	
-	//user_nameの表示
-	String user_name;
-	
 	public String getUser_name() {
 		return user_name;
 	}
-	
 	public void setUser_name(String user_name) {
 		this.user_name = user_name;
 	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getAddress3() {
+		return address3;
+	}
+	public void setAddress3(String address3) {
+		this.address3 = address3;
+	}
 	
-	//user_idの表示
-		String user_id;
-		
-		public String getUser_id() {
-			return user_id;
-		}
-		
-		public void setUser_id(String user_id) {
-			this.user_id = user_id;
-		}
 	
 }
 	
---
+++ src/main/java/com/example/Human/service/UserInfo.java
@@ -0,0 +1,51 @@
+package com.example.Human.service;
+
+public class UserInfo {
+	private String userId;
+	private String userName;
+	private String password;
+	private String address1;
+	private String address2;
+	private String address3;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getAddress3() {
+		return address3;
+	}
+	public void setAddress3(String address3) {
+		this.address3 = address3;
+	}
+
+	
+	
+
+}
